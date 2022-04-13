f27f879e06a9a7535f45fdb7d546dba1411ea6a7
larss2 <91117749+larss2@users.noreply.github.com>
2022-04-13 11:32:13 +0200

    Add files via upload

---
+++ Us7.php
@@ -0,0 +1,30 @@
+<?Php 
+include_once("usterstory7_functies.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="opmaak.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UserStory 7</title>
+</head>
+<body class="body">
+    <?php
+    Navigatiebar("us7");
+    ?>
+    <center>
+        <div>
+            <img class="logo" src="images/logo.png">
+        </div>
+        <div class="tekst">
+            <h1>Welkom op mijn website over Minecraft.</h1>
+            <h2>Je kunt op deze website 'crafting recipes', 'main characters' en de beesten bekijken.</h2>
+            <h2>Minecraft's oorspronkelijke datum van uitgave was op 18 november 2011.</h2>
+        </div>
+        <div>
+            <h2 class="credit">Website gemaakt door Lars Leijssen</h2>
+        </div>
+    </center>
+</body>
+</html>
